0698b0e6e0d3e6f37cf312f55c068fbe597c92d3
Jayin Ton <tonjayin@gmail.com>
2015-12-20 00:42:38 +0800

    1.0.0

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+'use strict'
+var request = require('request')
+
+if(process.argv.lenth < 3){
+    console.log('please input a url')
+    return
+}
+
+request('http://url2markdown.herokuapp.com/?url='+process.argv[2], function(error, response, body) {
+    if(error){
+        console.log(error)
+    }
+    if (!error && response.statusCode == 200) {
+        console.log(body) // Show the HTML for the Google homepage.
+    }
+})
